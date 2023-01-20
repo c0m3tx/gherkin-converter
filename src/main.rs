e83f02d8bcb86511d1350e660b2da456d0e8b972
Fabio Vitale <cometspk@gmail.com>
2023-01-20 11:23:21 +0100

    More accurate feature/scenario parsing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct Scenario {
 
 #[derive(Debug)]
 struct Feature {
-    name: String,
+    name: Option<String>,
     scenarios: Vec<Scenario>,
 }
 
@@ -44,31 +44,61 @@ fn write_output(features: Vec<Feature>, mut writer: impl Write) -> Result<(), st
     writer.write_all(output_content.as_bytes())
 }
 
-fn parse_scenario(input: &str) -> Scenario {
-    let scenario_name = input.lines().next().unwrap().trim().to_string();
-    let steps = input
-        .lines()
-        .skip(1)
-        .map(|l| l.trim().to_string())
-        .filter(|l| !l.is_empty())
+fn parse_scenario(input: &[&str]) -> Scenario {
+    let name = input[0].trim().to_string().replace("Scenario: ", "");
+    let steps = input[1..input.len()]
+        .iter()
+        .filter_map(|&s| (!s.is_empty()).then_some(s.trim().to_owned()))
         .collect();
-    Scenario {
-        name: scenario_name,
-        steps,
-    }
+
+    Scenario { name, steps }
 }
 
-fn parse_input(input: String) -> Vec<Feature> {
-    // parse the input into a list of features
-    input
-        .split("Feature:")
-        .filter(|s| !s.is_empty())
-        .map(|fc| {
-            let name = fc.lines().next().unwrap().trim().to_string();
-            let scenarios = fc.split("Scenario:").skip(1).map(parse_scenario).collect();
-            Feature { name, scenarios }
-        })
-        .collect()
+fn parse_feature(name: Option<String>, input: &[&str]) -> Feature {
+    let scenario_rows: Vec<_> = input
+        .iter()
+        .enumerate()
+        .filter(|(_, l)| l.trim().starts_with("Scenario:"))
+        .map(|(i, _)| i)
+        .chain(std::iter::once(input.len()))
+        .collect();
+
+    let scenarios = scenario_rows
+        .windows(2)
+        .map(|window| (window[0], window[1]))
+        .map(|(start, end)| parse_scenario(&input[start..end]))
+        .collect();
+
+    Feature { name, scenarios }
+}
+
+fn parse_input(input: &str) -> Vec<Feature> {
+    let input: Vec<_> = input.lines().collect();
+
+    let feature_rows: Vec<_> = input
+        .iter()
+        .enumerate()
+        .filter(|(_, l)| l.trim().starts_with("Feature:"))
+        .map(|(i, _)| i)
+        .chain(std::iter::once(input.len()))
+        .collect();
+
+    if feature_rows.len() == 1 {
+        vec![parse_feature(None, &input)]
+    } else {
+        feature_rows
+            .windows(2)
+            .map(|window| (window[0], window[1]))
+            .map(|(start, end)| {
+                let name = input[start].trim().to_string();
+                let name = name
+                    .contains("Feature: ")
+                    .then_some(name.replace("Feature: ", ""));
+                let feature = &input[start..(end - start)];
+                parse_feature(name, feature)
+            })
+            .collect()
+    }
 }
 
 impl Feature {
@@ -78,7 +108,10 @@ impl Feature {
         let scenarios: Vec<_> = self.scenarios.iter().map(Scenario::to_markdown).collect();
         let scenarios = scenarios.join("\n");
 
-        vec![format!("## {}", self.name), empty_row.clone(), scenarios].join("\n")
+        match &self.name {
+            Some(name) => vec![format!("## {}", name), empty_row.clone(), scenarios].join("\n"),
+            None => scenarios,
+        }
     }
 }
 
@@ -99,7 +132,7 @@ impl Scenario {
 
 fn main() {
     let input = read_input(std::env::args().nth(1), std::io::stdin()).unwrap();
-    let features = parse_input(input);
+    let features = parse_input(&input);
     write_output(features, std::io::stdout()).expect("Unable to write")
 }
 
@@ -124,9 +157,26 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_input() {
+        let features = parse_input(TEST_INPUT);
+        assert_eq!(features.len(), 1);
+        let feature = &features[0];
+        assert_eq!(feature.name, Some("feature name".to_string()));
+        let scenario = &feature.scenarios[0];
+        assert_eq!(scenario.name, "scenario name");
+        let steps = &scenario.steps;
+        assert_eq!(steps.len(), 3);
+
+        let scenario_2 = &feature.scenarios[1];
+        assert_eq!(scenario_2.name, "another scenario");
+        let steps = &scenario_2.steps;
+        assert_eq!(steps.len(), 4);
+    }
+
+    #[test]
     fn test_to_markdown() {
         let feature = Feature {
-            name: "Some feature".to_string(),
+            name: Some("Some feature".to_string()),
             scenarios: vec![
                 Scenario {
                     name: "Some scenario".to_string(),
